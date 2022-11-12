894384dd101883220468d318a211a66e73fb5147
hadiokinar <118039051+hadiokinar@users.noreply.github.com>
2022-11-12 12:46:58 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = "hadiokinar.py",
     version = "0.1",
-    py_modules = ['hadiokinar'],
+    py_modules = ['hadiokinar1'],
     url = "https://github.com/hadiokinar/hadiokinar1.git",
     author = "hadiokinar",
     install_requires = ["pyngrok"]
